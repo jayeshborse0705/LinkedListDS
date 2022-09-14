77b39b41938cbd43ec6c04f2d9c4eeef29c0ab27
jayesh <jayeshborse0705@gmail.com>
2022-09-13 20:25:58 -0700

    [jayesh] to create linked list

---
+++ LinkedList/LinkedList/Linklist.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Linklist
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} insterted into link list", node.data);
+
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("linklist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+       
+        
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to linklist operation:");
+            Linklist list = new Linklist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Display();
+        }
+    }
+}
